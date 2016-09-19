60ac58830f7a2bb80969daadb990dfe4f6f60679
Anton <anton.paskannyi@mail.ru>
2016-09-19 13:36:47 +0300

    Finished introduction to redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JavaScript: The Good Parts', pages: 12 },
+    { title: 'Harry Potter', pages: 2 },
+    { title: ' The Dark Tower', pages: 4 },
+    { title: 'ABC', pages: 6 }
+  ]
+}
